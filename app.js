1ffe2fb3431bc4e744739d04a8930b23f700c097
Simon Jokanic <simon05@tutamail.com>
2022-11-02 17:25:33 +0100

    The framework was built / CSS and JS built as well

---
+++ app.js
@@ -0,0 +1,4 @@
+const section = document.querySelectorAll('.selection');
+const sectButtons = document.querySelectorAll('.controls');
+const secBtn = document.querySelectorAll('.control');
+const allSelect = document.querySelectorAll('.main-content');
